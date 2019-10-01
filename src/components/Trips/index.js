3f8df554929de6b931a2d1a218c7ae51d6b28be3
raythurman2386 <raymondthurman5@gmail.com>
2019-10-01 13:55:47 -0400

    filter working, styles will need updated

--- src/components/Trips/index.js
+++ src/components/Trips/index.js
@@ -3,18 +3,22 @@ import response from '../../response/response'
 import Card from '../Card'
 
 const Trips = () => {
-    const [trips, setTrips] = useState(response)
+    const [allTrips] = useState(response)
+    const [trips, setTrips] = useState(allTrips)
 
     // Handle change for the filter functionality
     const handleChange = e => {
-        setTrips(trips.filter(item => item.category === e.target.value))
+        setTrips(allTrips.filter(item => item.category === e.target.value && item))
+        if (e.target.value === 'all') {
+            setTrips(allTrips)
+        }
     }
 
     return (
         <div className="trips-wrap">
             <h4 className="heading">view latest trips</h4>
             <select name="trips" onChange={e => handleChange(e)}>
-                <option value="">filter</option>
+                <option value="all">Filter</option>
                 <option value="adventure">Adventure</option>
                 <option value="family">Family</option>
                 <option value="weekend break">Weekend Break</option>
