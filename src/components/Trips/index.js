10782b965f75620ea1a7362eba4cc1e25eb178ee
raythurman2386 <raymondthurman5@gmail.com>
2019-10-01 12:21:50 -0400

    filter initially working

--- src/components/Trips/index.js
+++ src/components/Trips/index.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import response from '../../response/response'
 import Card from '../Card'
 
-const Trips = () => (
-  <div className="trips-wrap">
-    <h4 className="heading">view latest trips</h4>
-    <div className="trips-cards-wrap">
-      {response.map(trip => (
-        <Card props={trip} />
-      ))}
-    </div>
-  </div>
-)
+const Trips = () => {
+    const [trips, setTrips] = useState(response)
+
+    // Handle change for the filter functionality
+    const handleChange = e => {
+        setTrips(trips.filter(item => item.category === e.target.value))
+    }
+
+    return (
+        <div className="trips-wrap">
+            <h4 className="heading">view latest trips</h4>
+            <select name="trips" onChange={e => handleChange(e)}>
+                <option value="">filter</option>
+                <option value="adventure">Adventure</option>
+                <option value="family">Family</option>
+                <option value="weekend break">Weekend Break</option>
+            </select>
+            <div className="trips-cards-wrap">
+                {trips.map(trip => (
+                    <Card props={trip} />
+                ))}
+            </div>
+        </div>
+    )
+}
 
 export default Trips
